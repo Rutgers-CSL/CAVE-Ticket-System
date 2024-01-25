ba471df8a95c6990fe20907f57b59c303096cce7
ShatakshiRanjan <shatakshiranjan02@gmail.com>
2024-01-25 12:17:43 -0500

    last commit

--- Main.py
+++ Main.py
@@ -18,13 +18,12 @@ def display_tickets():
 
     # Get column values
     a_column_values = sh.col_values(1) 
-    b_column_values = sh.col_values(2)
-    c_column_values = sh.col_values(3)
+    b_column_values = sh.col_values(3)
+    c_column_values = sh.col_values(4)
 
     # Get current date and time
     current_datetime = datetime.now()
     formatted_datetime = current_datetime.strftime("%#m/%#d/%Y")
-    test_day = "1/7/2024"
 
     # Find the minimum length of both columns
     min_length = min(len(a_column_values), len(c_column_values))
@@ -36,8 +35,9 @@ def display_tickets():
         b_value = b_column_values[i]
         c_value = c_column_values[i]
 
-        if a_value == 'FALSE' and (b_value[:-9] == test_day or b_value[:-8] == test_day):
+        if a_value == 'FALSE' and (b_value[:-9] == formatted_datetime or b_value[:-8] == formatted_datetime):
             tickets_to_display.append({"c_value": c_value})
+
 
     # Render the template with the filtered tickets
     return render_template('display_tickets.html', tickets=tickets_to_display)
